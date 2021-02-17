012171df85de893cb3071109a23c855ff002db72
dawid m. wojciechowski <de.wojciechowski@gmail.com>
2021-02-17 19:02:08 +0100

    component start state

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,26 +3,31 @@ import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
 import Column from '../Column/Column.js';
+import {settings} from '../../data/dataStore';
+import ReactHtmlParser from 'react-html-parser';
 
 class List extends React.Component {
+  
+  state = {
+    columns: this.props.columns || [],
+  }
+
   static propTypes = {
     title: PropTypes.node.isRequired,
     image: PropTypes.string.isRequired,
-    children: PropTypes.node,
+    description: PropTypes.node,
     columns: PropTypes.array,
   }
 
   static defaultProps = {
-    children: <p>I can do all the things!!!</p>,
+    description: settings.defaultListDescription,
   }
 
   render() {
     return (
         <section className={styles.component}>
         <Hero titleText = {this.props.title} image = {this.props.image} />
-        <div className={styles.description}>
-          {this.props.children}
-        </div>
+        <div className={styles.description}>{ReactHtmlParser(this.props.description)}</div>
         <div className = {styles.columns}>
           <Column title = {'Animals'}></Column>
           <Column title = {'Plants'}></Column>
