384d876cfbfeea09955150f48ef9d8c3aa1eab3f
dawid m. wojciechowski <de.wojciechowski@gmail.com>
2021-02-20 09:08:58 +0100

    module 11 finished with hamburger

---
+++ src/components/Hamburger/Hamburger.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Hamburger.scss';
+
+class Hamburger extends React.Component {
+
+    propTypes = {
+        children: PropTypes.node
+    }
+
+    state = {
+        contentActive: false
+    }
+
+    handleClick = () => {
+        this.setState({ contentActive: !this.state.contentActive})
+    }
+    
+    render() {
+        return (
+            <div>
+                <a onClick={this.handleClick} class={styles.icon + " fas fa-bars"}></a>
+                <div className={styles.content + ((this.state.contentActive) ? ' ' + styles.contentActive : '')}>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Hamburger;
