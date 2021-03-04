f0145b8cbc6365354116c0a3827696d38db762b4
Dawid Wojciechowski <de.wojciechowski@gmail.com>
2021-03-04 12:49:25 +0100

    13.4 without improvement

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -1,12 +1,15 @@
 import {connect} from 'react-redux';
 import SearchResults from './SearchResults';
 import {getCardsForSearchResults} from '../../redux/cardsRedux';
+import { getColumnsForList } from '../../redux/columnsRedux';
 
 const mapStateToProps = (state, props) => {
-  const searchString = props.match.params.searchString;
+  const searchString = props.match.params.id;
 
   return {
     cards: getCardsForSearchResults(state, searchString),
+    columns: getColumnsForList(state, searchString ),
+    lists: state.lists,
   };
 };
 
