efb6e7a2dac3b2f87ef180b560ebcef5bd1d2bcb
Dawid Wojciechowski <de.wojciechowski@gmail.com>
2021-03-05 08:40:35 +0100

    disable droppable like a business Janusz

--- src/components/SearchResults/SearchResultsContainer.js
+++ src/components/SearchResults/SearchResultsContainer.js
@@ -1,15 +1,13 @@
 import {connect} from 'react-redux';
 import SearchResults from './SearchResults';
 import {getCardsForSearchResults} from '../../redux/cardsRedux';
-import { getColumnsForList } from '../../redux/columnsRedux';
+
 
 const mapStateToProps = (state, props) => {
-  const searchString = props.match.params.id;
+  const searchString = props.match.params.searchString;
 
   return {
     cards: getCardsForSearchResults(state, searchString),
-    columns: getColumnsForList(state, searchString ),
-    lists: state.lists,
   };
 };
 
